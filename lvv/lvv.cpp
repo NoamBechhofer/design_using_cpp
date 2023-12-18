17a8c70d52773cd786aec51c222a7a5050c78c2e
NoamBechhofer <noam.bechhofer@gmail.com>
2023-12-18 17:18:03 -0500

    formatting

--- lvv/lvv.cpp
+++ lvv/lvv.cpp
@@ -2,10 +2,10 @@
  * @file lvv.cpp
  * @brief Compare the performance of list<int> and vector<int>
  * @author Noam Bechhofer
- * 
+ *
  * @details
  * # list v. vector
- * 
+ *
  * - Exercise (as posed to me by John Bentley many years ago):
  * - Generate N random integers and insert them into a sequence so that each is
  *   inserted in its proper position in the numerical order. **5 1 4 2** gives:
@@ -13,26 +13,26 @@
  *         - 1 5
  *         - 1 4 5
  *         - 1 2 4 5
- * - Remove elements one at a time by picking a random position in the sequence and
- *   removing the element there. Positions **1 2 0 0** gives
+ * - Remove elements one at a time by picking a random position in the sequence
+ *   and removing the element there. Positions **1 2 0 0** gives
  *         - 1 2 4 5
  *         - 1 4 5
  *         - 1 4
  *         - 4
- * - For which N is it better to use a linked list than a vector (or an array) to
- *   represent the sequence?
+ * - For which N is it better to use a linked list than a vector (or an array)
+ *   to represent the sequence?
  * - The sequence grows incrementally
  * - Use **vector** and **list**. Use the same algorithms for both vector
  *   and list. Do not use **advance()** to traverse a sequence, write an
  *   explicit loop (**advance()** will do optimizations that will perturb your
  *   results). The N random numbers should come from a uniform distribution, but
- *   the numbers need to be distinct (i.e., duplicate values not are allowed; that
- *   implies that you’ll have to generate the sequence first and then use it. That
- *   also saves you from the mistake of measuring the cost of generating the
- *   initial random sequence as part of inserting and removing its elements from
- *   the ordered sequence).
- * - Use **<random>** to generate random integers and **<chrono>** for timing. Run
- *   each experiment three times with different seeds for the random number
+ *   the numbers need to be distinct (i.e., duplicate values not are allowed;
+ *   that implies that you’ll have to generate the sequence first and then use
+ *   it. That also saves you from the mistake of measuring the cost of
+ *   generating the initial random sequence as part of inserting and removing
+ *   its elements from the ordered sequence).
+ * - Use **<random>** to generate random integers and **<chrono>** for timing.
+ *   Run each experiment three times with different seeds for the random number
  *   generator to make it likely that your numbers are not perturbed by external
  *   factors.
  * - Draw a graph of the times for various numbers of elements (e.g., for 100K,
@@ -40,22 +40,23 @@
  *   drawing from a program.
  * - Do the experiment again using a **set**, and add its performance to the
  *   graph (to see the effect of using an O(log n) algorithm).
- * - Run at least one complementary experiment to help you understand what you see.
- *   For example, store the integers in a large struct to see the effects of size,
- *   pre-allocate the list elements to see the cost of allocation, or something.
- * - Write an explanation of why your results are the way they are – this write-up
- *   is essential, do not just send code and a graph.
- * - The code needs to be submitted together with the results. Try not to write C
- *   or Java in C++ - if you do, you’ll end up writing too much code.
- * - Due **Wednesday October 12**. I recommend to start early; people trying to do
- *   this exercise the last evening have often been unpleasantly surprised about
- *   the time needed.
+ * - Run at least one complementary experiment to help you understand what you
+ *   see. For example, store the integers in a large struct to see the effects
+ *   of size, pre-allocate the list elements to see the cost of allocation, or
+ *   something.
+ * - Write an explanation of why your results are the way they are – this
+ *   write-up is essential, do not just send code and a graph.
+ * - The code needs to be submitted together with the results. Try not to write
+ *   C or Java in C++ - if you do, you’ll end up writing too much code.
+ * - Due **Wednesday October 12**. I recommend to start early; people trying to
+ *   do this exercise the last evening have often been unpleasantly surprised
+ *   about the time needed.
  */
 
+#include "lvv.h"
 #include <cassert>
-#include <climits>
-
 #include <chrono>
+#include <climits>
 #include <fstream>
 #include <future>
 #include <iostream>
@@ -69,8 +70,6 @@
 #include <unordered_set>
 #include <vector>
 
-#include "lvv.h"
-
 constexpr int MAX_TESTS = 1'000'000;
 constexpr int DEFAULT_NUM_TESTS = 10'000;
 
@@ -78,89 +77,87 @@ using namespace std;
 
 namespace db {
 
-// NUM_INTS == MAX_TESTS is not a rule of nature, just how I did it
-constexpr int NUM_INTS = MAX_TESTS;
-const string INT_DB_REL_PATH = "./random_ints.txt";
+    // NUM_INTS == MAX_TESTS is not a rule of nature, just how I did it
+    constexpr int NUM_INTS = MAX_TESTS;
+    const string INT_DB_REL_PATH = "./random_ints.txt";
 
-/**
- * @brief try to read a set of integers from the database
- * 
- * @see #INT_DB_REL_PATH
- * 
- * @return optional<unordered_set<int>> the set read from the database, or
- * nullopt if the database could not be opened
- */
-optional<unordered_set<int>> read_int_db()
-{
-    ifstream int_db;
-    int_db.open(INT_DB_REL_PATH);
-    if (!int_db) {
-        return {};
+    /**
+     * @brief try to read a set of integers from the database
+     *
+     * @see #INT_DB_REL_PATH
+     *
+     * @return optional<unordered_set<int>> the set read from the database, or
+     * nullopt if the database could not be opened
+     */
+    optional<unordered_set<int>> read_int_db()
+    {
+        ifstream int_db;
+        int_db.open(INT_DB_REL_PATH);
+        if(!int_db) { return {}; }
+
+        unordered_set<int> s;
+        s.reserve(NUM_INTS);
+
+        string line;
+        while(getline(int_db, line)) { s.insert(stoi(line)); }
+        return s;
     }
-
-    unordered_set<int> s;
-    s.reserve(NUM_INTS);
-
-    string line;
-    while (getline(int_db, line)) {
-        s.insert(stoi(line));
-    }
-    return s;
-}
 
 } // namespace db
 
 /**
  * @brief fetch a set of integers to use for testing
- * 
+ *
  * @param min_num_ints the minimum number of integers to fetch
  * @return unordered_set<int> the set of integers to use for testing
  */
 unordered_set<int> fetch_int_set(int min_num_ints)
 {
-    return db::read_int_db().value_or(utils::generate_n_random_ints(min_num_ints, INT_MIN, INT_MAX));
+    return db::read_int_db().value_or(
+        utils::generate_n_random_ints(min_num_ints, INT_MIN, INT_MAX));
 }
 /** A set of random integers */
 const unordered_set<int> INT_SET = fetch_int_set(db::NUM_INTS);
 
 /**
- * @brief INTERNAL: the timed test function. The caller will be timing this function, so
- *        it should not do any blocking behavior
+ * @brief INTERNAL: the timed test function. The caller will be timing this
+ * function, so it should not do any blocking behavior
  */
-void inline test_n_core_(IntegerSequence& seq, size_t num_vals, const vector<size_t>& removal_indices)
+void inline test_n_core_(IntegerSequence& seq, size_t num_vals,
+                         const vector<size_t>& removal_indices)
 {
-    // assert(INT_SET.size() >= test_num && removal_indices.size() == test_num);
+    // assert(INT_SET.size() >= test_num && removal_indices.size() == test_num)
     auto itr = INT_SET.begin();
-    for (size_t i = 0; i < num_vals; i++) {
-        seq.insert_numerical(*itr++);
-    }
-    for (size_t i : removal_indices) {
-        seq.remove(i);
-    }
+    for(size_t i = 0; i < num_vals; i++) { seq.insert_numerical(*itr++); }
+    for(size_t i: removal_indices) { seq.remove(i); }
 }
 
 /**
  * @brief INTERNAL: driver function for #test_n. Runs synchronously
  * (directly returns the result) and asynchronously (sets the result on the
  * supplied promise)
- * 
+ *
  * @param seq the sequence to test
  * @param num_vals the number of values to insert and remove
- * @param removal_indices a vector dictating the order in which to remove elements.
+ * @param removal_indices a vector dictating the order in which to remove
+ * elements.
  * @details removal_indices[i] is the index of the element to remove on the ith
  *          iteration. So for example, given sequence {1, 2, 4, 5} and removal
  *          indices {1, 2, 0, 0}, the sequence would be reduced to {1, 4, 5},
- *          then {1, 4}, then {4}, then {}. * 
+ *          then {1, 4}, then {4}, then {}. *
  * @param promise the promise to set the result on
  * @param num_runs how many times to run the test
- * 
+ *
  * @return chrono::nanoseconds the average time it took to run the test
  */
-chrono::nanoseconds test_n_(IntegerSequence& seq, size_t num_vals, const vector<size_t>& removal_indices, promise<chrono::nanoseconds> promise, size_t num_runs = 3)
+chrono::nanoseconds test_n_(IntegerSequence& seq, size_t num_vals,
+                            const vector<size_t>& removal_indices,
+                            promise<chrono::nanoseconds> promise,
+                            size_t num_runs = 3)
 {
     chrono::nanoseconds avg{0};
-    for (size_t i = 0; i < num_runs; ++i) {
-        gen.seed(random_device {}());
+    for(size_t i = 0; i < num_runs; ++i) {
+        gen.seed(random_device{}());
 
         auto start = chrono::high_resolution_clock::now();
         test_n_core_(seq, num_vals, removal_indices);
@@ -175,28 +172,30 @@ chrono::nanoseconds test_n_(IntegerSequence& seq, size_t num_vals, const vector<
 }
 
 /**
- * @brief test the performance of a vector and a list for a specific N by 
+ * @brief test the performance of a vector and a list for a specific N by
  *        inserting and removing (in random order) num_vals elements
- * 
+ *
  * @param num_vals number of elements to insert and remove
  * @param num_runs how many times to run the test
- * 
+ *
  * @return pair<chrono::nanoseconds, chrono::nanoseconds> the average time it
- *        took to run the test for the vector and the list, respectively
+ *         took to run the test for the vector and the list, respectively
  */
-pair<chrono::nanoseconds, chrono::nanoseconds> test_n(size_t num_vals, size_t num_runs)
+pair<chrono::nanoseconds, chrono::nanoseconds> test_n(size_t num_vals,
+                                                      size_t num_runs)
 {
     assert(INT_SET.size() >= num_vals);
 
     vector<size_t> removal_indices(num_vals);
     size_t end = num_vals - 1;
-    for (size_t start = 0; start < num_vals; start++)  {
+    for(size_t start = 0; start < num_vals; start++) {
         removal_indices[start] = utils::random_int(0, (int)end);
         end--;
     }
     assert(removal_indices[removal_indices.size() - 1] == 0); // sanity check
 
-    // agh now we've duplicate dknowledge because num_vals == removal_indices.size()
+    // ! agh now we've duplicated knowledge because
+    // ! num_vals == removal_indices.size()
 
     VectorAdaptor v{};
     ListAdaptor l{};
@@ -207,8 +206,10 @@ pair<chrono::nanoseconds, chrono::nanoseconds> test_n(size_t num_vals, size_t nu
     future<chrono::nanoseconds> vec_future = vec_promise.get_future();
     future<chrono::nanoseconds> list_future = list_promise.get_future();
 
-    jthread vec_thread(test_n_, ref(v), num_vals, ref(removal_indices), move(vec_promise), num_runs);
-    jthread list_thread(test_n_, ref(l), num_vals, ref(removal_indices), move(list_promise), num_runs);
+    jthread vec_thread(test_n_, ref(v), num_vals, ref(removal_indices),
+                       move(vec_promise), num_runs);
+    jthread list_thread(test_n_, ref(l), num_vals, ref(removal_indices),
+                        move(list_promise), num_runs);
 
     chrono::nanoseconds vec_duration = vec_future.get();
     chrono::nanoseconds list_duration = list_future.get();
@@ -218,55 +219,53 @@ pair<chrono::nanoseconds, chrono::nanoseconds> test_n(size_t num_vals, size_t nu
 
 /**
  * @brief perform #test_n for a range of values
- * 
+ *
  * @param start the first value to test
  * @param end the last value to test
  * @param output the output stream to write to
  */
 void test_block(size_t start, size_t end, ostream& output)
 {
-    for (size_t i = start; i < end; i++) {
+    for(size_t i = start; i < end; i++) {
         auto [vec_duration, list_duration] = test_n(i, DEFAULT_RUNS_PER_TEST);
 
-        output << i 
-            << "," << vec_duration.count() 
-            << "," << list_duration.count() 
-            << "," << list_duration.count() - vec_duration.count() 
-            << endl;
+        output << i << "," << vec_duration.count() << ","
+               << list_duration.count() << ","
+               << list_duration.count() - vec_duration.count() << endl;
     }
 }
 
 /**
  * @brief parse command line arguments and return the number of tests to run
- * 
+ *
  * @param argv command line arguments
- * @return size_t the number of tests to run (#DEFAULT_NUM_TESTS if no argument is found)
+ * @return size_t the number of tests to run (#DEFAULT_NUM_TESTS if no argument
+ *         is found)
  */
 size_t lvv_parse_args(vector<string> argv)
 {
     size_t argc = argv.size();
-    if (argc < 1) {
+    if(argc < 1) {
         // to my understanding, this doesn't happen
         assert(false);
         exit(1);
     }
-    if (argc == 1) {
-        return DEFAULT_NUM_TESTS;
-    }
-    if (argc > 2) {
-        cerr << "Usage: " << argv[0] << " [optional: number of tests to run]" << endl;
+    if(argc == 1) { return DEFAULT_NUM_TESTS; }
+    if(argc > 2) {
+        cerr << "Usage: " << argv[0] << " [optional: number of tests to run]"
+             << endl;
         exit(1);
     }
     assert(argc == 2);
 
     int tests = stoi(argv[1]);
 
-    if (tests < 0) {
+    if(tests < 0) {
         cerr << "Number of tests must be non-negative" << endl;
         exit(1);
     }
 
-    if (tests > MAX_TESTS) {
+    if(tests > MAX_TESTS) {
         cerr << "Number of tests must be less than " << MAX_TESTS << endl;
         exit(1);
     }
--- lvv/lvv.h
+++ lvv/lvv.h
@@ -2,7 +2,6 @@
 #define LVV_H
 
 #include <cassert>
-
 #include <chrono>
 #include <future>
 #include <list>
@@ -10,90 +9,85 @@
 #include <unordered_set>
 #include <vector>
 
-
-/** 
- * @brief A random number generator 
- * @details be aware that this global generator is reseeded at points 
+/**
+ * @brief A random number generator
+ * @details be aware that this global generator is reseeded at points
  */
-std::mt19937 gen{std::random_device {}()};
+std::mt19937 gen{std::random_device{}()};
 
 constexpr int DEFAULT_RUNS_PER_TEST = 3;
 
 namespace utils {
 
-/**
- * @brief generate a uniformly distributed random integer between min and max (inclusive)
- *
- * @param min minimum value (inclusive)
- * @param max maximum value (inclusive)
- * @return int a random integer between min and max (inclusive)
- */
-int random_int(int min, int max)
-{
-    auto dist = std::uniform_int_distribution<>(min, max);
-    return dist(gen);
-}
-
-/**
- * @brief generate a set of n uniformly distributed random integers between min and max (inclusive)
- *
- * @param n number of integers to generate
- * @param min minimum value (inclusive)
- * @param max maximum value (inclusive)
- * @param gen random number generator
- * @return set<int> a set of n uniformly distributed random integers between min and max (inclusive)
- */
-std::unordered_set<int> generate_n_random_ints(size_t n, int min, int max)
-{
-    std::unordered_set<int> s;
-    while (s.size() < n) {
-        s.insert(random_int(min, max));
-    }
-    return s;
-}
-
-/**
- * @brief insert n into v in numerical order
- *
- * @param v vector to insert into
- * @param n value to insert
- */
-void insert_in_numerical_order(std::vector<int>& v, int n)
-{
-    if (v.empty()) {
-        v.push_back(n);
-        return;
+    /**
+     * @brief generate a uniformly distributed random integer between min and
+     * max (inclusive)
+     *
+     * @param min minimum value (inclusive)
+     * @param max maximum value (inclusive)
+     * @return int a random integer between min and max (inclusive)
+     */
+    int random_int(int min, int max)
+    {
+        auto dist = std::uniform_int_distribution<>(min, max);
+        return dist(gen);
     }
 
-    auto it = v.begin();
-    while (it != v.end() && *it < n) {
-        ++it;
-    }
-    v.insert(it, n);
-}
-
-/**
- * @brief insert n into l in numerical order
- *
- * @param l list to insert into
- * @param n value to insert
- */
-void insert_in_numerical_order(std::list<int>& l, int n)
-{
-    if (l.empty()) {
-        l.push_back(n);
-        return;
+    /**
+     * @brief generate a set of n uniformly distributed random integers between
+     * min and max (inclusive)
+     *
+     * @param n number of integers to generate
+     * @param min minimum value (inclusive)
+     * @param max maximum value (inclusive)
+     * @param gen random number generator
+     * @return set<int> a set of n uniformly distributed random integers between
+     * min and max (inclusive)
+     */
+    std::unordered_set<int> generate_n_random_ints(size_t n, int min, int max)
+    {
+        std::unordered_set<int> s;
+        while(s.size() < n) { s.insert(random_int(min, max)); }
+        return s;
     }
 
-    auto it = l.begin();
-    while (it != l.end() && *it < n) {
-        ++it;
+    /**
+     * @brief insert n into v in numerical order
+     *
+     * @param v vector to insert into
+     * @param n value to insert
+     */
+    void insert_in_numerical_order(std::vector<int>& v, int n)
+    {
+        if(v.empty()) {
+            v.push_back(n);
+            return;
+        }
+
+        auto it = v.begin();
+        while(it != v.end() && *it < n) { ++it; }
+        v.insert(it, n);
     }
-    l.insert(it, n);
-}
+
+    /**
+     * @brief insert n into l in numerical order
+     *
+     * @param l list to insert into
+     * @param n value to insert
+     */
+    void insert_in_numerical_order(std::list<int>& l, int n)
+    {
+        if(l.empty()) {
+            l.push_back(n);
+            return;
+        }
+
+        auto it = l.begin();
+        while(it != l.end() && *it < n) { ++it; }
+        l.insert(it, n);
+    }
 
 } // namespace utils
-
 
 /**
  * @brief abstract base class for a sequence of integers
@@ -140,7 +134,8 @@ public:
     virtual ~IntegerSequence() = default;
 
     /**
-     * @brief fill the sequence incrementally with the values in s in numerical order
+     * @brief fill the sequence incrementally with the values in s in numerical
+     * order
      *
      * @param s the set of values to insert
      * @param num_vals the number of values to insert
@@ -152,7 +147,7 @@ public:
         assert(s.size() >= num_vals);
 
         auto itr = s.begin();
-        for (size_t i = 0; i < num_vals; ++i) {
+        for(size_t i = 0; i < num_vals; ++i) {
             insert_numerical(*itr);
             ++itr;
         }
@@ -165,41 +160,23 @@ public:
 class ListAdaptor : public IntegerSequence {
 private:
     std::list<int> l;
-
 public:
-    explicit ListAdaptor(std::list<int> const& l)
-        : l(l)
-    {
-    }
+    explicit ListAdaptor(std::list<int> const& l): l(l) {}
     ListAdaptor() = default;
     void insert_numerical(int n) override
     {
         utils::insert_in_numerical_order(l, n);
     }
-    void push_back(int n) override
-    {
-        l.push_back(n);
-    }
-    void push_front(int n) override
-    {
-        l.push_front(n);
-    }
+    void push_back(int n) override { l.push_back(n); }
+    void push_front(int n) override { l.push_front(n); }
     void remove(size_t i) override
     {
         auto it = l.begin();
-        for (size_t j = 0; j < i; ++j) {
-            ++it;
-        }
+        for(size_t j = 0; j < i; ++j) { ++it; }
         l.erase(it);
     }
-    size_t size() override
-    {
-        return l.size();
-    }
-    bool empty() override
-    {
-        return l.empty();
-    }
+    size_t size() override { return l.size(); }
+    bool empty() override { return l.empty(); }
     ~ListAdaptor() override = default;
 };
 
@@ -209,41 +186,22 @@ public:
 class VectorAdaptor : public IntegerSequence {
 private:
     std::vector<int> v;
-
 public:
-    explicit VectorAdaptor(std::vector<int> const& v)
-        : v(v)
-    {
-    }
+    explicit VectorAdaptor(std::vector<int> const& v): v(v) {}
     VectorAdaptor() = default;
     void insert_numerical(int n) override
     {
         utils::insert_in_numerical_order(v, n);
     }
-    void push_back(int n) override
-    {
-        v.push_back(n);
-    }
-    void push_front(int n) override
-    {
-        v.insert(v.begin(), n);
-    }
-    void remove(size_t i) override
-    {
-        v.erase(v.begin() + i);
-    }
-    size_t size() override
-    {
-        return v.size();
-    }
-    bool empty() override
-    {
-        return v.empty();
-    }
+    void push_back(int n) override { v.push_back(n); }
+    void push_front(int n) override { v.insert(v.begin(), n); }
+    void remove(size_t i) override { v.erase(v.begin() + i); }
+    size_t size() override { return v.size(); }
+    bool empty() override { return v.empty(); }
     ~VectorAdaptor() override = default;
 };
 
-extern std::pair<std::chrono::nanoseconds, std::chrono::nanoseconds> test_n(size_t num_vals, size_t num_runs = DEFAULT_RUNS_PER_TEST);
-
+extern std::pair<std::chrono::nanoseconds, std::chrono::nanoseconds>
+test_n(size_t num_vals, size_t num_runs = DEFAULT_RUNS_PER_TEST);
 
 #endif // LVV_H
